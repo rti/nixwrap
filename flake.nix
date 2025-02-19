ac32ab10508ebebd8dc285ceecb95c5cf07289a3
Robert Timm <mail@rtti.de>
2025-02-19 05:02:53 +0000

    test: add first checks

--- flake.nix
+++ flake.nix
@@ -13,13 +13,34 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
-      {
+      rec {
+
         lib = import ./lib.nix { inherit nixpkgs; };
+
         packages = rec {
           wrap = pkgs.callPackage ./package.nix { };
           default = wrap;
         };
+
         devShells.default = import ./shell.nix { inherit pkgs; };
+
+        checks =
+          let
+            wrap-bin = "${packages.wrap}/bin/wrap";
+            bash-bin = "${pkgs.bash}/bin/bash";
+          in
+          {
+            env-home-is-always-exposed = pkgs.runCommand "env-home-is-always-exposed" { } ''
+              HOME=/homedir ${wrap-bin} ${bash-bin} -c 'echo $HOME' | grep homedir > $out
+            '';
+            user-name-is-hidden = pkgs.runCommand "user-name-is-hidden" { } ''
+              ${wrap-bin} whoami 2> error-msg || true
+              cat error-msg | grep "cannot find name for user ID" > $out
+            '';
+            user-name-is-exposed = pkgs.runCommand "user-name-is-exposed" { } ''
+              ${wrap-bin} -u whoami > $out
+            '';
+          };
       }
     );
 }
