efa082dc122da829a91870e1176b9aaece3a5382
Robert Timm <mail@rtti.de>
2025-02-19 03:11:30 +0000

    style: format

--- lib.nix
+++ lib.nix
@@ -1,10 +1,14 @@
-{ pkgs ? import <nixpkgs> { }, ... }:
+{
+  pkgs ? import <nixpkgs> { },
+  ...
+}:
 
 {
   wrap =
-    { package
-    , wrapArgs ? ""
-    , executable ? package.pname
+    {
+      package,
+      wrapArgs ? "",
+      executable ? package.pname,
     }:
 
     pkgs.symlinkJoin {
@@ -18,24 +22,4 @@
         chmod a+x $out/bin/${executable}
       '';
     };
-
-  # wrap = prog:
-  #   prog.overrideAttrs (oldAttrs:
-  #     let
-  #       name = prog.pname;
-  #     in
-  #     {
-  #       # TODO: why do we need to use postInstall? why is installPhase $out wrong?
-  #       postInstall = oldAttrs.postInstall or "" + ''
-  #         mv $out/bin/${name} $out/bin/${name}-nowrap
-  #         cat << _EOF > $out/bin/${name}
-  #         #! ${super.runtimeShell} -e
-  #         exec ${./wrap.sh} \
-  #           -- \
-  #           ${name}-nowrap "\$@"
-  #         _EOF
-  #         chmod 0755 $out/bin/${name}
-  #       '';
-  #     });
-  #
 }
